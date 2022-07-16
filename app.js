c45150f094671fd615f1976218b29da0d5497b98
Bacon9873 <daniel_markusson@yahoo.com>
2022-07-16 19:26:26 -0400

    c

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var db = require('../../models/data-model')
+var db = require('./models/data-model')
 
 var app = express();
 
@@ -13,33 +13,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, './index.html'))
+    res.status(200).sendFile(path.join(__dirname, './public/html/index.html'))
 })
 
 app.get('/addperson/info', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, './infoofperson.html'))
+    res.status(200).sendFile(path.join(__dirname, './public/html/infoofperson.html'))
 })
 
 app.get('/addperson/added', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, './personadded.html'))
+    res.status(200).sendFile(path.join(__dirname, './public/html/personadded.html'))
 })
 
 app.get('/inprogress', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, './inprogress.html'))
+    res.status(200).sendFile(path.join(__dirname, './public/html/inprogress.html'))
 })
 
 app.post('/addperson/add', (req, res) => {
     db.insertPerson(req.body.fname, req.body.lname, req.body.age, req.body.desc)
-    res.status(200).sendFile(path.join(__dirname, './personadded.html'))
+    res.status(200).sendFile(path.join(__dirname, './public/html/personadded.html'))
 })
 
 app.get('/play', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, './playmenu.html'))
+    res.status(200).sendFile(path.join(__dirname, './public/html/playmenu.html'))
 })
 
 app.get('/play/play', (req, res) => {
     db.getInfo()
-    res.status(200).sendFile(path.join(__dirname, './play.html'))
+    res.status(200).sendFile(path.join(__dirname, './public/html/play.html'))
 })
 
 app.get('/favicon.ico', (req, res) => {
